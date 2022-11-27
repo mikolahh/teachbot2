4fc0c30a6a208635e6d595b3c885e62ef55115e9
Mikalai Khadanovich <mikolahh@gmail.com>
2022-11-27 17:16:41 +0300

    ch50

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ ini_set('display_startup_errors', 1);
 // Функция для красивого вывода переменных на экран
 function prv($var)
 {
-   static $int = 0;
-   echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
-   var_dump($var);
-   // print_r($var);
-   echo '</pre>';
-   $int++;
-   echo '<br><br><br>';
+	static $int = 0;
+	echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
+	var_dump($var);
+	// print_r($var);
+	echo '</pre>';
+	$int++;
+	echo '<br><br><br>';
 }
 define("tg_token", "5653090500:AAFR7_OQ6-d_I0A8t-uItzW17sEf3PTFiXg");
 // здесь может быть как id отдельного пользователя, так и группы, куда будет отправляться сообщение
@@ -34,22 +34,22 @@ $textMessage = 'А теперь сообщение с кнопками!';
 
 // Массив для передачи параметров в сообщение
 $getQuery = array(
-   "chat_id" => tg_user_id,
-   "text" => $textMessage,
-   "reply_markup" => json_encode(
-   		array(
-   			'inline_keyboard' => array(
-   				array(array('text'=>'1','callback_data'=>'key1'),array('text'=>'2','callback_data'=>'key2'),array('text'=>'3','callback_data'=>'key3')),
-   				array(array('text'=>'4','callback_data'=>'key4'),array('text'=>'5','callback_data'=>'key5'),array('text'=>'6','callback_data'=>'key6')),
-   				array(array('text'=>'7','callback_data'=>'key7'),array('text'=>'8','callback_data'=>'key8'),array('text'=>'9','callback_data'=>'key9')),
-   				array(array('text'=>'*','callback_data'=>'key*'),array('text'=>'0','callback_data'=>'key0'),array('text'=>'#','callback_data'=>'key#'))
-   			),
-   			'keyboard' => array(
-   				array('text'=>'Действие1','callback_data'=>'do-1'),array('text'=>'Действие1','callback_data'=>'do-1'),
-   				array('text'=>'Действие1','callback_data'=>'do-1'),array('text'=>'Действие1','callback_data'=>'do-1')
-   				)
-   		)
-   )    
+	"chat_id" => tg_user_id,
+	"text" => $textMessage,
+	"reply_markup" => json_encode(
+		array(
+			'inline_keyboard' => array(
+				array(array('text' => '1', 'callback_data' => 'key1'), array('text' => '2', 'callback_data' => 'key2'), array('text' => '3', 'callback_data' => 'key3')),
+				array(array('text' => '4', 'callback_data' => 'key4'), array('text' => '5', 'callback_data' => 'key5'), array('text' => '6', 'callback_data' => 'key6')),
+				array(array('text' => '7', 'callback_data' => 'key7'), array('text' => '8', 'callback_data' => 'key8'), array('text' => '9', 'callback_data' => 'key9')),
+				array(array('text' => '*', 'callback_data' => 'key*'), array('text' => '0', 'callback_data' => 'key0'), array('text' => '#', 'callback_data' => 'key#'))
+			),
+			'keyboard' => array(
+				array(array('text' => 'Действие1', 'callback_data' => 'do-1'), array('text' => 'Действие1', 'callback_data' => 'do-1')),
+				array(array('text' => 'Действие1', 'callback_data' => 'do-1'), array('text' => 'Действие1', 'callback_data' => 'do-1'))
+			)
+		)
+	)
 );
 // формируем шаблон строки запроса на передачу сообщения
 $ch = curl_init(apiUrl . tg_token . "/sendMessage?" . http_build_query($getQuery));
