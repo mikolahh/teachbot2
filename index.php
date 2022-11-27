e620db972477239b24693a7222655cfa8bf706de
Mikalai Khadanovich <mikolahh@gmail.com>
2022-11-27 14:57:34 +0300

    ch39

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
-<?php 
+<?php
 
 // Код для вывлда ошибок
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 // Функция для красивого вывода переменных на экран
-function prv($var) {
-	static $int = 0;
-	echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
-	var_dump($var);
-	// print_r($var);
-	echo '</pre>';
-	$int++;
-	echo '<br><br><br>';
+function prv($var)
+{
+   static $int = 0;
+   echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
+   var_dump($var);
+   // print_r($var);
+   echo '</pre>';
+   $int++;
+   echo '<br><br><br>';
 }
 define("tg_token", "5653090500:AAFR7_OQ6-d_I0A8t-uItzW17sEf3PTFiXg");
 // здесь может быть как id отдельного пользователя, так и группы, куда будет отправляться сообщение
@@ -22,7 +23,7 @@ define("apiUrl", "https://api.telegram.org/bot");
 $getBotUpdate = apiUrl . tg_token . "/getUpdates";
 
 // Получаем обновления от бота через интересную функцию
-// $lastUpdates = file_get_contents($getBotUpdate);
+$lastUpdates = file_get_contents($getBotUpdate);
 
 // Выводим полученные обновления
 echo "Последние обновления, вывод через нашу специальную функцию: ";
@@ -33,9 +34,9 @@ $textMessage = 'Очередное тестовое сообщение из ур
 
 // Массив для передачи параметров в сообщение
 $getQuery = array(
-	"chat_id" => tg_user_id,
-	"text" => $textMessage,
-	"parse_mode" => "html",
+   "chat_id" => tg_user_id,
+   "text" => $textMessage,
+   "parse_mode" => "html",
 );
 // формируем шаблон строки запроса на передачу сообщения
 $ch = curl_init(apiUrl . tg_token . "/sendMessage?" . http_build_query($getQuery));
@@ -52,15 +53,4 @@ curl_close($ch);
 echo "Выводим результат запроса: ";
 prv(json_decode($resultQuery));
 
-
-
-
-
-
-
-
-
-
-
-
- ?>
+// ssh mikalayt@vh116.hoster.by -p 22   ew8nieKo
