1a310a739b109a7886416f5555726378d15eb706
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-04 13:06:54 +0300

    ch61

--- index.php
+++ index.php
@@ -1,49 +1,48 @@
 <?php
-// Код для вывлда ошибок
+// Код для вывода ошибок
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 // Функция для красивого вывода переменных на экран
 function prv($var)
 {
-	static $int = 0;
-	echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
-	var_dump($var);
-	// print_r($var);
-	echo '</pre>';
-	$int++;
-	echo '<br><br><br>';
+   static $int = 0;
+   echo '<pre><b style="background: blue;padding: 1px 5px;">' . $int . '</b> ';
+   var_dump($var);
+   // print_r($var);
+   echo '</pre>';
+   $int++;
+   echo '<br><br><br>';
 }
 define("tg_token", "5653090500:AAFR7_OQ6-d_I0A8t-uItzW17sEf3PTFiXg");
-// здесь может быть как id отдельного пользователя, так и группы, куда будет отправляться сообщение
-define("tg_user_id", -881391231);
+// здесь может быть как id отдельного пользователя, так и группы,
+//  куда будет отправляться сообщение
+define("tg_group_id", -881391231);
 define("apiUrl", "https://api.telegram.org/bot");
-// Попробуем сформировать переменную для плучения обновлений от бота
-$getBotUpdate = apiUrl . tg_token . "/getUpdates";
+define("botUrl", "https://mikalay.tech/bot/index.php");
 
-// Получаем обновления от бота через интересную функцию
-$lastUpdates = file_get_contents($getBotUpdate);
-
-// Выводим полученные обновления
-echo "Последние обновления, вывод через нашу специальную функцию: ";
-prv(json_decode($lastUpdates, true));
-
-$arrayQuery = array(
-	'chat_id' => tg_user_id,
-	'caption' => 'Проверка работоспособности отправки файлов с помощью хэша',
-	// 'photo' => curl_file_create(__DIR__ . '/diplom.jpg', 'image/jpg', 'diplom.jpg')
-	'document' => 'BQACAgIAAxkDAANqY4xZuq6oyos8R08KIhFkkhwh3NUAAmIfAAIotWFIESrkglsYRXwrBA'
+// Формируем массив с параметрами запроса на установку хука
+$getQuery = array(
+   "url" => botUrl,
 );
-// $ch = curl_init(apiUrl . tg_token . '/sendPhoto');
-$ch = curl_init(apiUrl . tg_token . '/sendDocument');
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $arrayQuery);
+// формируем сам запрос на установку хука с поощью curl
+$ch = curl_init(apiUrl . tg_token . "/setWebhook?" . http_build_query($getQuery));
+// Прописываем параметры curl
+
+// Чтобы curl не выводил результат, а возвращал его
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Запрещаем проверку ssl-сертификата удаленного сервера
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+// Исключаем из результата заголовки
 curl_setopt($ch, CURLOPT_HEADER, false);
-$res = curl_exec($ch);
+
+// Вызываем запрос
+$resultQuery = curl_exec($ch);
+
+// закрываем сессию
 curl_close($ch);
-prv(json_decode($res, true));
 
-
-// ssh mikalayt@vh116.hoster.by -p 22   ew8nieKo
+// Красиво выводим результаты
+prv($resultQuery);
