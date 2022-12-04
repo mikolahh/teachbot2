7048a4e26705bf7c09ef105b15d65c543afa8a3e
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-04 11:15:52 +0300

    ch58

--- index.php
+++ index.php
@@ -30,12 +30,12 @@ prv(json_decode($lastUpdates, true));
 
 $arrayQuery = array(
 	'chat_id' => tg_user_id,
-	'caption' => 'Проверка работоспособности отправки файлов',
-	//  'photo' => curl_file_create(__DIR__ . '/diplom.jpg', 'image/jpg' , 'diplom.jpg')
-	'document' => curl_file_create(__DIR__ . '/diplom.jpg', 'image/jpg', 'diplom.jpg')
+	'caption' => 'Проверка работоспособности отправки фото еще раз',
+	'photo' => curl_file_create(__DIR__ . '/diplom.jpg', 'image/jpg', 'diplom.jpg')
+	// 'document' => curl_file_create(__DIR__ . '/diplom.jpg', 'image/jpg', 'diplom.jpg')
 );
-// $ch = curl_init(apiUrl . tg_token .'/sendPhoto');
-$ch = curl_init(apiUrl . tg_token . '/sendDocument');
+$ch = curl_init(apiUrl . tg_token . '/sendPhoto');
+// $ch = curl_init(apiUrl . tg_token . '/sendDocument');
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $arrayQuery);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
