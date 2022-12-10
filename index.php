b9a2dcbc42b6a18eb425c45eecd5a7dc2637f613
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-11 00:50:51 +0300

    ch77

--- index.php
+++ index.php
@@ -29,9 +29,9 @@ $getQuery = array(
             array(array('text' => 'Действие1', 'callback_data' => 'do-1'), array('text' => 'Действие2', 'callback_data' => 'do-2')),
             array(array('text' => 'Действие3', 'callback_data' => 'do-3'), array('text' => 'Действие4', 'callback_data' => 'do-4'))
          ),
-         'keyboard' => array(
+         /*'keyboard' => array(
             array(array('text' => 'Start', 'callback_data' => '/start'), array('text' => 'Reserve', 'callback_data' => '/reserve'))
-         )
+         )*/
 
       )
    )
