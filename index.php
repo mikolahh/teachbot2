e787b417dc5c9d3f48ce1fe959872d14432830fc
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-13 19:02:22 +0300

    ch

--- index.php
+++ index.php
@@ -53,8 +53,8 @@ require_once "myfunctions/delete_hook.php";
 // writeLogFile($data, true);
 // $res = setMyHook();
 // prv($res);
-// require "myfunctions/write_log.php";
-function writeLogFile($string, $clear = false)
+include "myfunctions/write_log.php";
+/* function writeLogFile($string, $clear = false)
 {
    $log_file_name = __DIR__ . "/message.txt";
    $now = date("Y-m-d H:i:s");
@@ -64,7 +64,7 @@ function writeLogFile($string, $clear = false)
       file_put_contents($log_file_name, '');
       file_put_contents($log_file_name, $now . " " . print_r($string, true) . "\r\n", FILE_APPEND);
    }
-}
+} */
 writeLogFile("hello mikola", true);
 // deleteMyHook();
 
