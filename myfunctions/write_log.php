7fe40af0563d7e2164f42cd272ecef3676e90772
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-30 18:41:29 +0300

    ch128

--- myfunctions/write_log.php
+++ myfunctions/write_log.php
@@ -4,10 +4,10 @@ function writeLogFile($string, $clear = false)
     $log_file_name = __DIR__ . "/message.txt";
     $now = date("Y-m-d H:i:s");
     $data = $string;
-    if ($clear == false) {
+    if ($clear === false) {
         file_put_contents($log_file_name, $now . " " . print_r($data, true) . "\r\n", FILE_APPEND);
     } else {
-        // file_put_contents($log_file_name, '');
+        file_put_contents($log_file_name, '');
         file_put_contents($log_file_name, $now . " " . print_r($data, true) . "\r\n", FILE_APPEND);
     }
 }
