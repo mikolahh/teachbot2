61da795f1741165237afbad3b804b091e8a2ed72
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-30 14:55:05 +0300

    ch121

--- myfunctions/write_log.php
+++ myfunctions/write_log.php
@@ -2,7 +2,6 @@
 function writeLogFile($string, $clear = false)
 {
     $log_file_name = __DIR__ . "/message.txt";
-    prv($log_file_name);
     $now = date("Y-m-d H:i:s");
     if ($clear == false) {
         file_put_contents($log_file_name, $now . " " . print_r($string, true) . "\r\n", FILE_APPEND);
