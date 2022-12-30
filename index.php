3537b5b63c80a746565e4a261b9361eb2cc72017
Mikalai Khadanovich <mikolahh@gmail.com>
2022-12-30 19:19:57 +0300

    ch139

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ function writeLogFile($string, $clear = false)
    if ($clear == false) {
       file_put_contents($log_file_name, $now . " " . print_r($data, true) . "\r\n", FILE_APPEND);
    } else {
-      // file_put_contents($log_file_name, '');
+      file_put_contents($log_file_name, '');
       file_put_contents($log_file_name, $now . " " . print_r($data, true) . "\r\n", FILE_APPEND);
    }
 }
